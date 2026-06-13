2ecfaf888c8a80331dbf010f6567285eddb0a531
MohitMeghwal <mohitmeghwal222@gmail.com>
2026-06-13 16:55:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = "ai-mohit-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open('moh1it-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
   );
 });
-
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
